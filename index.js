d7f17983fc6e60ba4209d1b027f5518724c0a320
willboyden <willboyden@gmail.com>
2020-03-19 18:47:50 -0400

    added redis cashing to cityVenues end point

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const fetch = require("node-fetch");
 //const sqlqry = require("mysql-mavents");
 const sqlqry = require("./mysql-mavents");
 const cors = require("cors");
@@ -13,6 +14,15 @@ app.use(compression());
 
 app.use(express.static("./build"));
 
+const redis = require("redis");
+// create and connect redis client to local instance.
+const client = redis.createClient(6379);
+
+// echo redis errors to the console
+client.on("error", err => {
+  console.log("Error " + err);
+});
+//console.log(client)
 var whitelist = [
   "https://localhost:44305",
   "https://localhost:44301",
@@ -40,20 +50,23 @@ const lightsaleConnection = mysql.createConnection({
   database: process.env.lightsaledbdatabase,
   port: 3306
 });
+
 // var connString =
 //   "mysql://express:Ocean2468#@ls-72718cbc1491523607dce5bcd6173f2b0d4897bf.c3zjlagatzuv.us-east-1.rds.amazonaws.com/mdb?charset=utf8_general_ci&timezone=-0700";
 
+//check connection
 // lightsaleConnection.connect(function(err) {
 //   if (err) throw err;
 //   console.log("Connected!");
 //   lightsaleConnection.end();
 // });
+
 //dynamically get port environment variable (set outside of application)
 //const port = process.env.PORT || 3000; //def to 3000 if envVar not set
 const port = 3000;
 //app.listen(port, () => console.log(`listening on port ${port}`));
 app.listen(port, () => console.log(`listening on port ${port} testing ${""}`));
-console.log(process.env.lightsaledbhost);
+//console.log(process.env.lightsaledbhost);
 //function for getting generic Query, can be used with regular string as param
 getQueryResultAsync = async function(sqlstr) {
   return new Promise(function(resolve, reject) {
@@ -85,6 +98,41 @@ respondQryResultAsync = async function(req, res, func, reqParams) {
       res.send("oops");
     });
 };
+
+app.get("/api/cityVenues/redis", cors(corsOptionsDelegate), (req, res) => {
+  // key to store results in Redis store
+  console.log("hit");
+  const photosRedisKey = "user:cityVenues";
+
+  // Try fetching the result from Redis first in case we have it cached
+  return client.get(photosRedisKey, (err, photos) => {
+    // If that key exists in Redis store
+    if (photos) {
+      console.log("success");
+      return res.json({ source: "cache", data: JSON.parse(photos) });
+    } else {
+      // Key does not exist in Redis store
+
+      // Fetch directly from remote api
+      fetch("http://52.2.126.232/api/cityVenues/")
+        .then(response => response.json())
+        .then(photos => {
+          // Save the  API response in Redis store,  data expire time in 3600 seconds, it means one hour
+          client.setex(photosRedisKey, 3600, JSON.stringify(photos));
+
+          console.log("saved to redis?");
+          // Send JSON response to client
+          return res.json({ source: "api", data: photos });
+        })
+        .catch(error => {
+          // log error message
+          console.log(error);
+          // send error to the client
+          return res.json(error.toString());
+        });
+    }
+  });
+});
 
 //run at domain
 app.get("/api/", cors(corsOptionsDelegate), async (req, res) => {
